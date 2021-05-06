b687be0edb5c6fb8a992769e9665399be7e1b3a2
MatthewWilsonBCAA <72146567+MatthewWilsonBCAA@users.noreply.github.com>
2021-05-06 14:48:45 -0500

    got started

---
+++ src/com/company/Entity.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Entity {
+    public String name;
+    int hp;
+    int maxHp;
+    int vitality;
+    int strength;
+    int dexterity;
+    int power;
+    int will;
+    int agility;
+    public Entity(String tName, int vit, int str, int dex, int pow, int wil, int agi) {
+        name = tName;
+        vitality = vit;
+        strength = str;
+        dexterity = dex;
+        power = pow;
+        will = wil;
+        agility = agi;
+        maxHp = vitality * 10;
+    }
+    public int getLevel() {
+        return vitality + strength + dexterity + power + will + agility;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Entity player = new Entity("player", 10, 10, 10, 10, 10, 10);
+	    System.out.println(player.getLevel());
+    }
+}
