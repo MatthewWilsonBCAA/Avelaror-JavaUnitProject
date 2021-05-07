428b507b2d28d85db82b55860bdb069ec7d428ca
MatthewWilsonBCAA <72146567+MatthewWilsonBCAA@users.noreply.github.com>
2021-05-07 11:59:38 -0500

    added list for items in rooms

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -4,4 +4,5 @@ public class Room {
     public String title;
     public String baseDescription;
     public ArrayList<Entity> entities = new ArrayList<Entity>();
+    public ArrayList<Item> items = new ArrayList<Item>();
 }
