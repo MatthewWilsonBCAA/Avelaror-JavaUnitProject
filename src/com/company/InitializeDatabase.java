0269499759b06d7642c2ab4cf4db64f55e25e4c9
MatthewWilsonBCAA <72146567+MatthewWilsonBCAA@users.noreply.github.com>
2021-05-13 16:20:05 -0500

    REEEEEEEEEEEEEEEEE$EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- src/com/company/InitializeDatabase.java
+++ src/com/company/InitializeDatabase.java
@@ -8,26 +8,49 @@ import java.util.Scanner;
 
 public class InitializeDatabase {
     static Scanner stdin = new Scanner(System.in);
-    static String dburl = "jdbc:sqlite:database.db";
+    static String dburl = "jdbc:sqlite:itemlist.db";
     static Connection conn;
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         connect();
-        //            var people = findAllPeople();
-//            for (Person person : people) {
-//                System.out.println(person.name);
-//            }
+        try {
+            var items = getItems();
+            for (Item i : items) {
+                System.out.println(i.name);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-//    private static ArrayList<Person> findAllPeople() throws SQLException {
-//        var people = new ArrayList<Person>();
-//        var statement = conn.createStatement();
-//        var results = statement.executeQuery("SELECT name FROM people");
-//        while (results.next()) {
-//            people.add(new Person(results.getString("name")));
-//        }
-//        return people;
-//    }
+    private static ArrayList<Item> getItems() throws SQLException {
+        System.out.println("Step 1...");
+        var items = new ArrayList<Item>();
+        System.out.println("Step 2...");
+        var statement = conn.createStatement();
+        System.out.println("Step 3...");
+        var results = statement.executeQuery("SELECT * FROM items");
+        System.out.println("Step 4...");
+        while (results.next()) {
+            items.add(new Item(
+                    results.getString("name"),
+                    results.getString("flavor_text"),
+                    results.getInt("effect"),
+                    results.getInt("effect_rating"),
+                    results.getInt("strength_req"),
+                    results.getFloat("strength_scale"),
+                    results.getInt("dexterity_req"),
+                    results.getFloat("dexterity_scale"),
+                    results.getInt("power_req"),
+                    results.getFloat("power_scale"),
+                    results.getInt("will_req"),
+                    results.getFloat("will_scale"),
+                    results.getFloat("weight"),
+                    results.getInt("value")
+            ));
+        }
+        return items;
+    }
 
     private static void connect() {
         try {
